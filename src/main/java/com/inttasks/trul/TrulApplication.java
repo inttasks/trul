4f9dec1e97269f6ce14e5c839379ed2431af2122
al166 <alibasirian.j@gmail.com>
2022-01-30 23:56:21 +0330

    init

---
+++ src/main/java/com/inttasks/trul/TrulApplication.java
@@ -0,0 +1,13 @@
+package com.inttasks.trul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrulApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrulApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/inttasks/trul/TrulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inttasks.trul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrulApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
